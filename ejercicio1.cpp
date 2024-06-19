3e6f0eab6054a1593a62cc0fbcb1ca286972fad6
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-06-18 23:55:04 -0500

    Actualización 2 del ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -13,18 +13,18 @@ int main(){
 	cout<<"Ingrese un numero: ";cin>>n;
 	
 	if(n == 0){
-		cout<<"El numero es nulo\n";
+		cout<<n<<" es nulo\n";
 	}
 	else if(n % 2 == 0){
-		cout<<"El numero es par\n";
+		cout<<n<<" es par\n";
 	}
 		else{
-			cout<<"El numero es impar\n";
+			cout<<n<<" es impar\n";
 		}
 		
 	system("pause");
 	return 0;
 	
-	}
+}
 		
 		
