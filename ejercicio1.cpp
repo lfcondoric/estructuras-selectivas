e15a72a18d80b7b6411935a06bbbf6e0e9fcea3b
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-06-21 22:42:55 -0500

    Actualización 4 del ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -11,21 +11,19 @@ int main(){
 	
 	int n;
 	
+	
+	cout<<"\t\t\tConsulte si es par o impar\n";
 	cout<<"Ingrese un numero: ";cin>>n;
 	
+	cout<<"Espere 3 segundos\n";
+	Sleep(3000);
 	if(n == 0){
-		cout<<"Espere 3 segundos\n";
-		Sleep(3000);
 		cout<<n<<" es nulo\n";
 	}
 	else if(n % 2 == 0){
-		cout<<"Espere 3 segundos\n";
-		Sleep(3000);
 		cout<<n<<" es par\n";
 	}
 		else{
-			cout<<"Espere 3 segundos\n";
-			Sleep(3000);
 			cout<<n<<" es impar\n";
 		}
 		
